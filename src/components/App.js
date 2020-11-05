12ce6946313c93c5b5eb942c56771db2216f8680
jayasankar123 <jayasankarreddy0@gmail.com>
2020-11-05 23:57:46 +0530

    firstVersion

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">Project NAME1</h1>
+      <p data-ns-test="project-description">Project description 1</p>
+      <h1 data-ns-test="project-name">Project NAME2</h1>
+      <p data-ns-test="project-description">Project description 2</p>
+    </>
+  );
+};
 
 export default App;
